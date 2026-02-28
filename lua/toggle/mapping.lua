69f7f58b91790766aa89097801d9b3975d92e084
Sasha Gurevich <sagurevich@microsoft.com>
2026-02-28 15:22:30 +0200

    feat(init.lua): main plugin code

---
+++ lua/toggle/mapping.lua
@@ -0,0 +1,28 @@
+local mapping = {}
+
+local M = {}
+
+M.__reset = function()
+    mapping = {}
+end
+
+M.__get_mapping = function(word)
+    return mapping[word]
+end
+
+M.__register = function(list)
+  for i, value in ipairs(list) do
+    if i == #list then
+      mapping[value] = list[1]
+    else
+      mapping[value] = list[i + 1]
+    end
+  end
+end
+
+M.__has_mapping = function(word)
+  return mapping[word] ~= nil
+end
+
+return M
+
