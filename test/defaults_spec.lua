90a8300f3054ab797b1a80df57e27c17165de300
Sasha Gurevich <sagurevich@microsoft.com>
2026-03-05 21:48:51 +0200

    test(defaults): added test for duplicate exclusion

--- test/defaults_spec.lua
+++ test/defaults_spec.lua
@@ -3,10 +3,15 @@ local defaults = require('toggle.defaults')
 
 describe('defaults', function()
     it('no duplicates in mappings', function()
-        -- TODO
-    end)
-
-    it('no duplicates in between mappings', function()
-        -- TODO
+        local seen = {}
+        for _, mapping in ipairs(defaults) do
+            for _, word in ipairs(mapping) do
+                if seen[word] then
+                    assert(false)
+                else
+                    seen[word] = true
+                end
+            end
+        end
     end)
 end)
