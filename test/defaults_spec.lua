3205e66b47e60144cdf44472bee8b08889e6f81d
Sasha Gurevich <sagurevich@microsoft.com>
2026-03-05 19:09:49 +0200

    test: test planning

---
+++ test/defaults_spec.lua
@@ -0,0 +1,12 @@
+
+local defaults = require('toggle.defaults')
+
+describe('defaults', function()
+    it('no duplicates in mappings', function()
+        -- TODO
+    end)
+
+    it('no duplicates in between mappings', function()
+        -- TODO
+    end)
+end)
